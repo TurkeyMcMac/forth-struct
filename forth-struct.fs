ab5b8546d5c7125b5b556938760c1c4b0a53ae07
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-01-29 14:14:45 -0500

    Initial commit

---
+++ forth-struct.fs
@@ -0,0 +1,45 @@
+( align size -- alignedsize )
+: ALIGNSIZE OVER + 1 - OVER / * ;
+
+( itemalign itemsize length -- arrayalign arraysize )
+: ARRAYFLD 2 PICK ROT ALIGNSIZE * ;
+
+( 1align 1size 2align 2size -- unionalign unionsize )
+: UNIONFLD ROT MAX >R MAX R> ;
+
+( 1align 1size ... nalign nsize n -- unionalign unionsize )
+: UNIONSFLD 1 ?DO UNIONFLD LOOP ;
+
+( -- cellalign cellsize )
+: CELLFLD 1 CELLS DUP ;
+
+( -- charalign charsize )
+: CHARFLD 1 CHARS DUP ;
+
+( -- 1 0 )
+: STRUCTURE: 1 0 ;
+
+( structalign structsize fieldalign fieldsize -- structalign' structsize' )
+: FIELDNAME
+	OVER 4 PICK MAX	>R
+	>R SWAP ALIGNSIZE
+	DUP R> + R> SWAP ROT
+	CREATE , DOES> @ +
+; 
+
+( structalign structsize -- structalign structsize )
+: STRUCTSIZE DUP CREATE , DOES> @ ;
+
+( structalign structsize structsize' -- structalign structsize' )
+: STRUCTSETSIZE NIP ;
+
+( structalign structsize -- structalign structsize )
+: STRUCTALIGN OVER CREATE , DOES> @ ;
+
+( structalign structsize structalign' -- structalign' structsize )
+: STRUCTSETALIGN ROT DROP SWAP ;
+
+: STRUCTEMBED 2DUP CREATE , , DOES> DUP CELL+ @ SWAP @ ;
+
+( structalign structsize -- )
+: ;STRUCTURE 2DROP ;
